2e7184145f54abd793e0ce60cc6465731334623a
UHKim <kimhr0703@gmail.com>
2017-12-11 14:20:14 +0900

    Spring Init

---
+++ src/main/java/com/unkim/springprocessing/SpringProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.unkim.springprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProcessingApplication.class, args);
+	}
+}
